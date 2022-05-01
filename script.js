0ed86b51eb364d3a87a6c7d94944414046ffcd99
mauromassari <98588586+mauromassari@users.noreply.github.com>
2022-05-01 16:47:14 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,82 @@
+fetch("./db.json") //fai una chiamata a questo indirizzo
+//then= "quando hai finito"
+
+.then((response) => response.json()) //trasforma la risposta alla chiamata in un JSON
+.then((data) => {
+  //quando hai finito di trasfrmare la risposta in JSON
+  //console.log("Success:", data); //in JSON fai le seguenti cose
+
+  data.lavori.forEach((element) => {
+    console.log(element.nome);
+
+    let divContenitorePadre = document.querySelector("#contenitorePadre");
+
+        let divLavoro = document.createElement("div");
+        divLavoro.classList.add("col");
+        divContenitorePadre.appendChild(divLavoro);
+
+            //Div Card
+            let divCard = document.createElement("div");
+            divCard.classList.add("card","h-100", "text-dark", "bg-light");
+            divLavoro.appendChild(divCard);
+
+            //Card Header
+            let divCardHeader = document.createElement("div");
+            divCardHeader.classList.add("card-header");
+            divCardHeader.innerText = "Tecnologia: " + element.tecnologia;
+            divCard.appendChild(divCardHeader);
+
+            //Img Card
+            let imgCard = document.createElement("img");
+            imgCard.classList.add("card-img-top");
+            imgCard.src = element.srcImgCard;
+            imgCard.setAttribute("alt", element.altImgCard);
+            imgCard.setAttribute("data-bs-toggle", "modal");
+            imgCard.setAttribute("data-bs-target", "#modal-" + element.id);
+            imgCard.setAttribute("type", "button");
+            divCard.appendChild(imgCard);
+
+            //Card Body
+            let divCardBody = document.createElement("div");
+            divCardBody.classList.add("card-body");
+            divCard.appendChild(divCardBody);
+
+                //Card Title
+                let divCardTitle = document.createElement("h5");
+                divCardTitle.classList.add("card-title");
+                divCardTitle.innerText = element.nomeLavoro;
+                divCardBody.appendChild(divCardTitle);
+
+                //Card Text
+                let divCardDescription = document.createElement("p");
+                divCardDescription.classList.add("card-text");
+                divCardDescription.innerText = element.textCard;
+                divCardBody.appendChild(divCardDescription);
+
+
+            //Card Footer
+            let divCardFooter = document.createElement("div");
+            divCardFooter.classList.add("card-footer");
+            divCard.appendChild(divCardFooter);
+
+            //Card Footer Button YouTube
+            let divCardFooterButtonYouTube = document.createElement("a");
+            divCardFooterButtonYouTube.classList.add("btn", "btn-outline-danger", "btn-sm");
+            divCardFooterButtonYouTube.setAttribute("href", element.linkYouTube);
+            divCardFooterButtonYouTube.setAttribute("target", "_blank");
+            divCardFooterButtonYouTube.setAttribute("style", "margin-right:10px;");
+            divCardFooterButtonYouTube.innerHTML = `<i class="bi bi-youtube"></i> Funzionalità`;
+            divCardFooter.appendChild(divCardFooterButtonYouTube);
+
+            //Card Footer Button YouTube
+            let divCardFooterButtonGithub = document.createElement("a");
+            divCardFooterButtonGithub.classList.add("btn", "btn-outline-dark", "btn-sm");
+            divCardFooterButtonGithub.setAttribute("href", element.linkGithub);
+            divCardFooterButtonGithub.setAttribute("target", "_blank");
+            divCardFooterButtonGithub.innerHTML = `<i class="bi bi-github"></i> GitHub`;
+            divCardFooter.appendChild(divCardFooterButtonGithub);
+        }
+    );
+    
+ 
+});
