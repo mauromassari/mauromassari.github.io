121fd1971c614c2b545cbd755eb04666ca6b64cf
Mauro Massari <98588586+mauromassari@users.noreply.github.com>
2022-06-12 10:25:08 +0200

    Add files via upload

---
+++ script/modali.js
@@ -0,0 +1,94 @@
+fetch("./db.json") //fai una chiamata a questo indirizzo
+//then= "quando hai finito"
+
+.then((response) => response.json()) //trasforma la risposta alla chiamata in un JSON
+.then((data) => {
+  //quando hai finito di trasfrmare la risposta in JSON
+  //console.log("Success:", data); //in JSON fai le seguenti cose
+
+  data.lavori.forEach((element) => {
+    console.log(element.nome);
+
+    //Array tecnologie Front End
+    let tecnologieDev = [
+        "Three.js",
+        "Angular",
+        "JavaScript",
+        "Ionic",
+        "Bootstrap",
+        "Sass e Bootstrap",
+        "jQuery",
+        "HTML",
+        "HTML e CSS",
+    ];
+
+
+    let divContenitoreModali = document.querySelector("#contenitoreModali");
+
+        let divModale = document.createElement("div");
+        divModale.classList.add("modal", "fade" );
+        divModale.setAttribute("id", "modal-" + element.id);
+        divModale.setAttribute("tabindex", "-1");
+        divModale.setAttribute("aria-hidden", "true");
+        divModale.setAttribute("aria-labelledby", "exampleModalLabel");
+        divContenitoreModali.appendChild(divModale);
+
+            let divModalDialog = document.createElement("div");
+            divModalDialog.classList.add("modal-dialog", "modal-xl");
+            divModale.appendChild(divModalDialog);
+
+                let divModalContent = document.createElement("div");
+                divModalContent.classList.add("modal-content"); 
+                divModalDialog.appendChild(divModalContent);
+
+                    let divModalHeader = document.createElement("div");
+                    divModalHeader.classList.add("modal-header");
+                    divModalContent.appendChild(divModalHeader);
+
+                        let modalH5 = document.createElement("h5");
+                        modalH5.classList.add("modal-title");
+                        modalH5.innerHTML = element.nomeLavoro;
+                        divModalHeader.appendChild(modalH5);
+
+                        let buttonClose = document.createElement("button");
+                        buttonClose.classList.add("btn-close");
+                        buttonClose.setAttribute("type", "button");
+                        buttonClose.setAttribute("data-bs-dismiss", "modal");
+                        buttonClose.setAttribute("aria-label", "Close");
+                        divModalHeader.appendChild(buttonClose);
+
+                let divModalBody = document.createElement("div");
+                divModalBody.classList.add("modal-body", "mx-auto");
+                divModalContent.appendChild(divModalBody);      
+                
+                    
+                let tec = tecnologieDev.find(tec => tec == element.categoria); 
+                    
+                    if(tec != undefined){
+                        let iframeModal = document.createElement("iframe");
+                        iframeModal.setAttribute("src", element.YoutubeModale );
+                        iframeModal.setAttribute("frameborder", "0");
+                        iframeModal.setAttribute("allow", "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture");
+                        iframeModal.setAttribute("title", "YouTube video player");
+                        iframeModal.setAttribute("width", "800");
+                        iframeModal.setAttribute("height", "500");
+                        iframeModal.setAttribute("allowfullscreen", "true");
+                        divModalBody.appendChild(iframeModal);
+                    }else{
+                        let iframeModal = document.createElement("iframe");
+                        iframeModal.setAttribute("src", element.presentazioneModale );
+                        iframeModal.setAttribute("frameborder", "0");
+                        iframeModal.setAttribute("allow", "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture");
+                        iframeModal.setAttribute("title", "YouTube video player");
+                        iframeModal.setAttribute("width", "800");
+                        iframeModal.setAttribute("height", "500");
+                        iframeModal.setAttribute("allowfullscreen", "true");
+                        divModalBody.appendChild(iframeModal); 
+                    }
+
+                    
+        }
+    );
+    
+ 
+});
---
+++ script/script.js
@@ -0,0 +1,131 @@
+fetch("./db.json") //fai una chiamata a questo indirizzo
+//then= "quando hai finito"
+
+.then((response) => response.json()) //trasforma la risposta alla chiamata in un JSON
+.then((data) => {
+  //quando hai finito di trasfrmare la risposta in JSON
+  //console.log("Success:", data); //in JSON fai le seguenti cose
+
+  data.lavori.forEach((element) => {
+    console.log(element.nome);
+
+    //Array tecnologie Front End
+    let categorieDev = [
+        "Three.js",
+        "Angular",
+        "JavaScript",
+        "Ionic",
+        "Bootstrap",
+        "Sass e Bootstrap",
+        "jQuery",
+        "HTML",
+        "HTML e CSS",
+    ];
+
+    //Array tecnologie Front End
+    let categorieMarketing = [
+        "Canva e Illustrator",
+        "SEM",
+        "SEO",
+        "Mailchimp",
+        "SEO Copywriting"
+    ];
+
+    let divContenitorePadre = document.querySelector("#contenitorePadre");
+
+        let divLavoro = document.createElement("div");
+        divLavoro.classList.add("col");
+        divLavoro.classList.add("hov");
+        //divLavoro.setAttribute("style", "display:none;");
+        divContenitorePadre.appendChild(divLavoro);
+
+            //Div Card
+            let divCard = document.createElement("div");
+            divCard.classList.add("card","h-100", "text-dark", "bg-light");
+            divLavoro.appendChild(divCard);
+
+            //Card Header
+            let divCardHeader = document.createElement("div");
+            divCardHeader.classList.add("card-header");
+            divCardHeader.innerText = "Categoria: " + element.categoria;
+            divCard.appendChild(divCardHeader);
+
+            //Img Card
+            let imgCard = document.createElement("img");
+            imgCard.classList.add("card-img-top");
+            imgCard.src = element.srcImgCard;
+            imgCard.setAttribute("alt", element.altImgCard);
+            imgCard.setAttribute("data-bs-toggle", "modal");
+            imgCard.setAttribute("data-bs-target", "#modal-" + element.id);
+            imgCard.setAttribute("type", "button");
+            divCard.appendChild(imgCard);
+
+            //Card Body
+            let divCardBody = document.createElement("div");
+            divCardBody.classList.add("card-body");
+            divCard.appendChild(divCardBody);
+
+                //Card Title
+                let divCardTitle = document.createElement("h5");
+                divCardTitle.classList.add("card-title");
+                divCardTitle.innerText = element.nomeLavoro;
+                divCardBody.appendChild(divCardTitle);
+
+                //Card Text
+                let divCardDescription = document.createElement("p");
+                divCardDescription.classList.add("card-text");
+                divCardDescription.innerText = element.textCard;
+                divCardBody.appendChild(divCardDescription);
+
+
+                categorieDev.forEach((tec) => {
+                    if(tec == element.categoria){
+                        //Card Footer
+                        let divCardFooter = document.createElement("div");
+                        divCardFooter.classList.add("card-footer");
+                        divCard.appendChild(divCardFooter);
+
+                        //Card Footer Button YouTube
+                        let divCardFooterButtonYouTube = document.createElement("a");
+                        divCardFooterButtonYouTube.classList.add("btn", "btn-outline-danger", "btn-sm");
+                        divCardFooterButtonYouTube.setAttribute("href", element.linkYouTube);
+                        divCardFooterButtonYouTube.setAttribute("target", "_blank");
+                        divCardFooterButtonYouTube.setAttribute("style", "margin-right:10px;");
+                        divCardFooterButtonYouTube.innerHTML = `<i class="bi bi-youtube"></i> Funzionalità`;
+                        divCardFooter.appendChild(divCardFooterButtonYouTube);
+
+                        //Card Footer Button GitHub
+                        let divCardFooterButtonGithub = document.createElement("a");
+                        divCardFooterButtonGithub.classList.add("btn", "btn-outline-dark", "btn-sm");
+                        divCardFooterButtonGithub.setAttribute("href", element.linkGithub);
+                        divCardFooterButtonGithub.setAttribute("target", "_blank");
+                        divCardFooterButtonGithub.innerHTML = `<i class="bi bi-github"></i> Codice`;
+                        divCardFooter.appendChild(divCardFooterButtonGithub);
+                    }
+                });
+
+
+                categorieMarketing.forEach((tec) => {
+                    if(tec == element.categoria){
+                        //Card Footer
+                        let divCardFooter = document.createElement("div");
+                        divCardFooter.classList.add("card-footer");
+                        divCard.appendChild(divCardFooter);
+
+                        //Card Footer Button GoogleDrive
+                        let divCardFooterButtonGoogleD = document.createElement("a");
+                        divCardFooterButtonGoogleD.classList.add("btn", "btn-outline-primary", "btn-sm");
+                        divCardFooterButtonGoogleD.setAttribute("href", element.linkGoogleDrive);
+                        divCardFooterButtonGoogleD.setAttribute("target", "_blank");
+                        divCardFooterButtonGoogleD.setAttribute("style", "margin-right:10px;");
+                        divCardFooterButtonGoogleD.innerHTML = `<i class="bi bi-cloud-arrow-down"></i> Download`;
+                        divCardFooter.appendChild(divCardFooterButtonGoogleD);
+                    }
+                });
+
+                
+    }
+    );
+    
+ 
+});
